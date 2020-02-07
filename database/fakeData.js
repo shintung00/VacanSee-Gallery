b67b92ec822f6aab642eb0bb54e457244a70d731
Hsin tung <hsintung@Hsins-MacBook-Pro.local>
2020-02-07 09:21:30 -0800

    includes seeding script

--- database/fakeData.js
+++ database/fakeData.js
@@ -1,60 +1,64 @@
-var faker = require('faker');
+const faker = require('faker');
+const schema = require('./schema.js');
+
+const { save } = schema;
 
 
-//Array of all the hosted images
-var imageBank = ['https://xillow.s3-us-west-1.amazonaws.com/house/image1.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image10.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image11.jpeg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image12.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image13.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image14.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image15.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image16.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image17.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image18.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image19.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image2.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image20.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image21.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image22.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image23.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image24.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image25.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image26.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image27.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image28.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image3.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image30.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image31.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image32.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image4.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image5.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image7.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image8.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image9.jpg'];
+// Array of all the hosted images
+let imageBank = ['https://xillow.s3-us-west-1.amazonaws.com/house/image1.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image10.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image11.jpeg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image12.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image13.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image14.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image15.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image16.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image17.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image18.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image19.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image2.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image20.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image21.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image22.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image23.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image24.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image25.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image26.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image27.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image28.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image3.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image30.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image31.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image32.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image4.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image5.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image7.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image8.jpg', 'https://xillow.s3-us-west-1.amazonaws.com/house/image9.jpg'];
 
 
-//Helper function to generate random number of images for a given num of houses
-var randomImagesArray = (num, images) => {
-  var result = [];
-  var imagesCopy = [...images];
-  for (var i = 0; i < num; i++) {
-    var randomNum = Math.floor(Math.random() * imagesCopy.length);
+// Helper function to generate random number of images for a given num of houses
+let randomImagesArray = (num, images) => {
+  let result = [];
+  let imagesCopy = [...images];
+  for (let i = 0; i < num; i++) {
+    let randomNum = Math.floor(Math.random() * imagesCopy.length);
     result.push(imagesCopy[randomNum]);
-    imagesCopy.splice(randomNum, 1); 
+    imagesCopy.splice(randomNum, 1);
   }
   return result;
 };
 
 
+let seedData = (number) => {
+  const result = [];
+  for (let i = 0; i < number; i++) {
+    let entry = {};
+    let address = '';
 
-var seedData = (number) => {
-  let result = [];
-  for (var i = 0; i < number; i++) {
-    var entry = {};
-    var address = '';
-  
-    //Generate random numbers for description
+    // Generate random numbers for description
     entry.description = {};
     entry.description.price = faker.random.number();
     entry.description.bdrm = faker.random.number();
     entry.description.bthrm = faker.random.number();
     entry.description.sqft = faker.random.number();
-  
-  
-    //Generate fake address
-    address += faker.address.streetAddress() + " ";
-    address += faker.address.streetName() + " ";
-    address += faker.address.city() + " ";
-    address += faker.address.state() + " ";
+
+
+    // Generate fake address
+    address += `${faker.address.streetAddress()  } `;
+    address += `${faker.address.streetName()  } `;
+    address += `${faker.address.city()  } `;
+    address += `${faker.address.state()  } `;
     address += faker.address.zipCode();
     entry.address = address;
-  
-  
-    //Generate fake images
-    var imageCount = Math.floor((Math.random() * 16) + 6);
-    var randomImages = randomImagesArray(imageCount, imageBank);
+
+
+    // Generate fake images
+    let imageCount = Math.floor((Math.random() * 16) + 6);
+    let randomImages = randomImagesArray(imageCount, imageBank);
     entry.images = randomImages;
-  
-    //Generate fake ID
+
+    // Generate fake ID
     entry.id = i;
-  
+
     result.push(entry);
-  };
+  }
   return result;
 };
 
+const sampleHouses = seedData(100);
+sampleHouses.forEach((house) => save(house));
 
+console.log(save);
 // module.exports = {seedData: seedData};
-
--- database/schema.js
+++ database/schema.js
@@ -1,47 +1,47 @@
 const mongoose = require('mongoose');
-const fakeData = require('./fakeData.js');
+// const fakeData = require('./fakeData.js');
 // const seedData = fakeData.seedData;
 
-//Create a new xillow db and drop if it exists
+// Create a new xillow db and drop if it exists
 mongoose.connect('mongodb://localhost/xillow');
-// mongoose.connection.dropDatabase();
+mongoose.connection.dropDatabase();
 
-//Schema
-let houseSchema = mongoose.Schema({
-  id: {type: Number, required: true, unique: true},
-  address: {type: String, required: true, unique: true},
-  images: [{type: String}],
-  description: String
+// Schema
+const houseSchema = mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
+  address: { type: String, required: true, unique: true },
+  images: [{ type: String }],
+  description: String,
 });
 
-//Model
-let House = mongoose.model('House', houseSchema);
+// Model
+const House = mongoose.model('House', houseSchema);
 
-//Save helper function
-// let save = (individualHouse) => {
-//   var newHouse = House({
-//     id: individualHouse.id,
-//     address: individualHouse.address,
-//     images: individualHouse.images,
-//     description: `Price: ${individualHouse.description.price.toString()} bedrooms: ${individualHouse.description.bdrm}  bathrooms: ${individualHouse.description.bthrm} sqft: ${individualHouse.description.sqft}`
-//   });
+// Save helper function
+const save = (individualHouse) => {
+  let newHouse = House({
+    id: individualHouse.id,
+    address: individualHouse.address,
+    images: individualHouse.images,
+    description: `Price: ${individualHouse.description.price.toString()} bedrooms: ${individualHouse.description.bdrm}  bathrooms: ${individualHouse.description.bthrm} sqft: ${individualHouse.description.sqft}`,
+  });
 
-//   newHouse.save((err, success) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log('House created!');
-//     }
-//   })
-// };
+  newHouse.save((err, success) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('House created!');
+    }
+  });
+};
 
-//Seed the mock data
+// Seed the mock data
 // let sampleHouses = seedData(100);
 // sampleHouses.forEach(house => save(house));
 
 
-//Retrieve helper function
-let retrieve = (req, res) => {
+// Retrieve helper function
+const retrieve = (req, res) => {
   House.find({}, (err, houses) => {
     if (err) {
       console.log(err);
@@ -49,7 +49,7 @@ let retrieve = (req, res) => {
     } else {
       res.send(houses);
     }
-  })
+  });
 };
 
-module.exports = {retrieve: retrieve};
+module.exports = { retrieve, save };
