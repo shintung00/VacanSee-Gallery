a3e3336fa60103d991c8c1cf44571aca527bfccf
Hsin tung <hsintung@Hsins-MacBook-Pro.local>
2020-02-08 21:49:59 -0800

    implement final

--- client/src/components/ViewerHeader.jsx
+++ client/src/components/ViewerHeader.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import ShareForm from './ShareForm.jsx';
 import styled from 'styled-components';
+// from submodule AK-service
+import ModalCA from '../../../AK-service/client/comp/ModalCA.jsx';
+import ModalTT from '../../../AK-service/client/comp/ModalTT.jsx';
+import { GreyOverlay } from '../../../AK-service/client/comp/style.jsx';
 
 const Exit = styled.button`
 color: white;
@@ -41,8 +45,8 @@ border: rgba(0,0,0,0);
 font-size: 16px;
 cursor: pointer;
 position: absolute;
-top: 6%;
-left: 81%;
+top: 11%;
+left: 80%;
 display: inline-block;
 outline:0;
 `;
@@ -99,9 +103,14 @@ class ViewerHeader extends React.Component {
     this.state = {
       popup: false,
       activeForm: '',
+      modalType: 0
     }
     this.togglePopout = this.togglePopout.bind(this);
     this.closePopout = this.closePopout.bind(this);
+    
+    // function about contact agent and take a tour button modal
+    this.showModalOnclick = this.showModalOnclick.bind(this);
+    this.hideModalOnclick = this.hideModalOnclick.bind(this);
   }
 
   togglePopout() {
@@ -118,17 +127,60 @@ class ViewerHeader extends React.Component {
     this.setState({ activeForm: formName });
   }
 
+  showModalOnclick(e) {
+    // decide which modal to show 
+    const whichModal = e.target.innerHTML.includes('Tour') ? 2 : 1;
+    
+    //  show modal
+    this.setState ({ modalType: whichModal });
+
+    // make other parts of the app clickable
+    document.addEventListener('click', this.hideModalOnclick, {once: true});
+  }
+
+  hideModalOnclick(e) {
+    if (e.target.classList[3] === "closeIcon") { // close button clicked
+      this.setState ({ modalType: 0 });
+    } else if (document.getElementById('summaryModal').contains(e.target)) { // modal other than close button clicked
+      // do not hide, but make app part clickable again
+      const target = document.getElementById('app');
+      target.addEventListener('click', this.hideModalOnclick, {once: true});
+    } else { // outside of popup clicked
+      this.setState ({ modalType: 0 });
+    }
+  }
+
+
   render() {
+    let modal;
+    switch(this.state.modalType) {
+      case 1: 
+        modal = (<span>
+                  <ModalCA hideModalOnclick={this.hideModalOnclick}/>
+                  <GreyOverlay id="greyOverlay" style={{width: innerWidth, height: innerHeight}}/>
+                </span>);
+        break;
+      case 2:
+        modal = (<span>
+                  <ModalTT hideModalOnclick={this.hideModalOnclick}/>
+                  <GreyOverlay id="greyOverlay" style={{width: innerWidth, height: innerHeight}}/>
+                </span>);
+        break;
+      default:
+        modal = '';
+    }
+
     return (
     <HeaderContainer>
       <PhotosTab onClick={this.props.restart}>Photos</PhotosTab>
-      <ScheduleTour onClick={(event) => this.formClick(event.target.id)} id="schedule">Schedule Tour</ScheduleTour>
+      <ScheduleTour onClick={this.showModalOnclick} id="schedule">Schedule Tour</ScheduleTour>
       <SaveHome onClick={this.props.save}>{this.props.saved ? '♥ Home Saved': '♡ Save Home'}</SaveHome>
-      <ShareHouse onClick={(event) => this.formClick(event.target.id)} id="schedule">✉️ Share</ShareHouse>
+      <ShareHouse onClick={this.showModalOnclick} id="schedule">Contact Agent</ShareHouse>
       <Exit onClick={this.props.close}>X</Exit>
     {this.state.activeForm === "schedule" && <ShareForm close={this.closePopout} />}
-    {/* {this.state.activeForm === "schedule" && <ScheduleForm close={this.closePopout}/>} */}
+    {modal}
     </HeaderContainer>
+    
     
     )
   }
